f1ba8189eebcfeec7b8a1f5b7fff5f5e72527e1e
Piyush <piyushsingh90@outlook.in>
2018-08-25 14:56:46 +0530

    adding login

--- auth.js
+++ auth.js
@@ -1,23 +1,23 @@
 var bcrypt = require('bcrypt-nodejs')
 var User = require('./models/User.js')
 var jwt = require('jwt-simple')
+var express  = require('express')
+var router = express.Router()
 
-module.exports = {
-    register: (req, res) => {
+router.post('/register', (req, res) => {
         var userData = req.body;
         
         var user = new User(userData)
-        user.save((err, result) => {
+        user.save((err, newUser) => {
             if (err)
-            console.log('Error while saving user')
-    
-            
-            res.sendStatus(200)
+            return res.status(500).send({message: 'Error saving user'});
+       
+            createSendToken(res, newUser)
         })
     
-    },
+})
 
-    login: async (req, res) => {
+router.post('/login', async (req, res) => {
         var loginData = req.body;
         
         var user = await User.findOne({email: loginData.email})
@@ -31,13 +31,36 @@ module.exports = {
                 return res.status(401).send({message: 'Email or password is invalid'});
             }
     
-            var payload = {}
-    
-            var token = jwt.encode(payload, '123')
-            
-            res.status(200).send({token})
+            createSendToken(res, user)
         })
-    }
+})
 
-
+function createSendToken(res, user){
+    var payload = {sub : user._id}
+    
+    var token = jwt.encode(payload, '123')
+    
+    res.status(200).send({token})
 }
+
+var auth = {
+    router,
+    checkAuthenticated: (req, res, next) =>{
+        if(!req.header('authorization')){
+            return res.status(401).send({message: 'Unauthorized'})
+        }
+    
+        var token = req.header('authorization').split(' ')[1]
+        var payload = jwt.decode(token, '123')
+    
+        if(!payload){
+            return res.status(401).send({message: 'Unauthorized'})
+        }
+    
+        req.userId = payload.sub
+    
+        next()
+    }
+}
+
+module.exports = auth
--- server.js
+++ server.js
@@ -6,22 +6,17 @@ var jwt = require('jwt-simple')
 var bcrypt = require('bcrypt-nodejs')
 
 var User = require('./models/User.js')
+var Post = require('./models/Post.js')
 var auth = require('./auth.js')
 
 var app = express()
 
 mongoose.Promise = Promise
 
-var posts = [
-    {message : "Hi"},
-    {message : "Hello"}
-]
-
 app.use(cors())
 app.use(bodyParser.json())
 
-
-app.get('/users',async  (req, res) => {
+app.get('/users', auth.checkAuthenticated, async  (req, res) => {
     try {
         var users = await User.find({}, '-pwd -__v')
         res.status(200).send(users)
@@ -41,16 +36,39 @@ app.get('/profile/:id',async  (req, res) => {
     }
 })
 
-app.get('/posts', (req, res) => {
-    res.send(posts)
+app.get('/posts/:id', async (req, res) => {
+    try {
+        var author = req.params.id;
+        var posts =await  Post.find({author});
+        res.status(200).send(posts);
+    } catch (error) {
+        console.error(error)
+        res.sendStatus(500)
+    }
 })
 
-app.post('/register', auth.register)
-app.post('/login', auth.login)
+app.post('/post', auth.checkAuthenticated, (req, res) => {
+    var postData = req.body;
+    postData.author = req.userId;
+
+    var post = new Post(postData)
+
+    post.save((err, result) => {
+        if (err){
+            
+        console.log('Error while saving post')
+        res.status(500).send('Saving Post Error')
+        }
+        
+        res.sendStatus(200)
+    })
+})
+
 
 mongoose.connect('mongodb://test:test123@ds121652.mlab.com:21652/mean', {useNewUrlParser: true}, (err) => {
     if (!err)
         console.log('connected to mongoDb')
 })
 
+app.use('/auth', auth.router)
 app.listen(process.env.PORT || 3000)
